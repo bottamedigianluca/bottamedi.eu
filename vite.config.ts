7a3d85861d73b6df4d52fa96c25f36090f85d37a
bottamedigianluca <bottamedigianluca@gmail.com>
2025-06-11 15:40:31 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -79,7 +79,8 @@ export default defineConfig({
       'react',
       'react-dom',
       'framer-motion',
-      'react-intersection-observer'
+      'react-intersection-observer',
+      'core-js'
     ]
   },
 

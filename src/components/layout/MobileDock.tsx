2b1aa4925461d0b0d7874866df4bb8fb0e25b8ec
bottamedigianluca <bottamedigianluca@gmail.com>
2025-06-20 00:11:34 +0200

    Update MobileDock.tsx

--- src/components/layout/MobileDock.tsx
+++ src/components/layout/MobileDock.tsx
@@ -128,11 +128,6 @@ return () => {
   window.removeEventListener('scroll', handleScroll)
   if (rafId) cancelAnimationFrame(rafId)
 }
-IGNORE_WHEN_COPYING_START
-content_copy
-download
-Use code with caution.
-IGNORE_WHEN_COPYING_END
 
 }, [isMobile, currentSection])
 
